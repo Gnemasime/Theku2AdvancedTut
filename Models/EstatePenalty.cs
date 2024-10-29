ad646fd9bdf47955abd2ed2675b4855b2c545647
22123521 <22123521@ITWINLAB-7>
2024-10-29 11:01:10 +0000

    New void

--- Models/EstatePenalty.cs
+++ Models/EstatePenalty.cs
@@ -23,46 +23,103 @@ namespace Theku2AdvancedTut.Models
         public virtual Area Area { get; set; }
         public virtual Violation Violation { get; set; }
 
-        public decimal ViolationCost()
+
+        //Method to pull violation cost
+        public decimal PullViolationCost()
         {
             ThekuDb db = new ThekuDb();
             var vcost = (from c in db.violations
                          where c.ViolationCode == ViolationCode
-                         select c.ViolationCode).FirstOrDefault();
+                         select c.ViolationCost).FirstOrDefault();
             return vcost;
         }
 
-        public decimal AreaRate()
+        //Method to pull area rate
+        public decimal PullAreaRate()
         {
             ThekuDb db = new ThekuDb();
             var rate = (from c in db.areas
                          where c.AreaCode == AreaCode
-                         select c.AreaCode).FirstOrDefault();
+                         select c.AreaRate).FirstOrDefault();
             return rate;
         }
 
         public decimal CalcAreaPenaltyCost()
         {
-            return ViolationCost() * (AreaRate() / 100.0m);
+            return PullViolationCost() * (PullAreaRate() / 100.0m);
         }
 
-        public int PullAge()
+        //Method to pull owner age
+        public int PullOwnerage()
         {
             ThekuDb db = new ThekuDb();
-            var age = (from a in db.owners
-                       where a.OwnerId == OwnerId
-                       select a.OwnerAge).FirstOrDefault();
+            var age = (from g in db.owners
+                       where g.OwnerId == OwnerId
+                       select g.OwnerAge).FirstOrDefault();
             return age;
         }
+
+
+        //Method to calculate age penalyt cost
         public decimal CalcAgePenaltyCost()
         {
-            if(PullAge() < 26)
+            if(PullOwnerage() < 26)
             {
-                return ViolationCost() * 0.05m;
+                return PullViolationCost() * (5 / 100.0m);
             }
             else
             {
-                return ViolationCost() * 0.03m;
+                return PullViolationCost() * (3 / 100.0m);
+            }
+        }
+
+
+        //Method to calculate total penalty cost
+        public decimal CalcTotalPenalyCost()
+        {
+            return CalcAgePenaltyCost() + CalcAreaPenaltyCost();
+        }
+
+        //method for points to deduct
+
+        public decimal CalcPointsToDeduct()
+        {
+            return Math.Floor(PullViolationCost() * (1 / 100.0m));
+        }
+
+        //Method that updates points
+        public void UpdatePoints()
+        {
+            ThekuDb db = new ThekuDb();
+            Owner owner = (from o in db.owners
+                           where o.OwnerId == OwnerId
+                           select o).FirstOrDefault();
+
+            owner.OwnerPoints -= Convert.ToInt32(CalcPointsToDeduct()); // owner.OwnerPoints = owner.OwnerPoints - Convert.ToInt32(CalcPointsToDeduct())
+
+            if(owner.OwnerPoints <= 0)
+            {
+                owner.OwnerPoints = 0;
+                owner.Status = "Invalid";
+            }
+            db.SaveChanges();
+        }
+
+        //Method To Check driver's License Validity
+        public bool CheckDrive()
+        {
+            ThekuDb db = new ThekuDb();
+            Owner owner = (from o in db.owners
+                           where o.OwnerId == OwnerId
+                           select o).FirstOrDefault();
+
+            if(owner.Status =="Valid")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
